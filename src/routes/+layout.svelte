31d0cbdd723bad6f0e9781d11ff0e7b963dbe205
mhigley <mhigley@bitwiseindustries.com>
2023-03-09 15:24:51 -0800

    Example Project Init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import '@picocss/pico'
+</script>
+
+<div class="container">
+    <hgroup>
+        <h1>
+            <a href="/">SvelteKit & Prisma</a>
+        </h1>
+        <h2>CRUD application w/ SvelteKit & Prisma</h2>
+    </hgroup>
+    <slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<div class="grid">
+    <div>
+        <h2>Articles:</h2>
+        <article>
+            <header>Example Article</header>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam eius repudiandae cum, nisi corporis eligendi rem non, ab dolor ipsum, recusandae hic distinctio unde adipisci.</p>
+            <button class="outline secondary">Delete Article</button>
+            <a href="/" role="button" class="outline contrast" style="width: 100%;">Edit Article</a>
+        </article>
+    </div>
+
+    <form action="?/createArticle" method="POST">
+        <h3>New Article</h3>
+        <label for="title">Title</label>
+        <input type="text" id="title" name="title" />
+        <label for="content">Content</label>
+        <textarea name="content" id="content" rows={5} />
+        <button type="submit">Add Article</button>
+    </form>
+</div>
